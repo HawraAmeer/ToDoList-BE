38de5ec1a5cfc5070152fbe1b360b14cbc0a9205
HawraAmeer <hwra.ameer97@gmail.com>
2021-02-14 14:40:16 +0300

    Model Needs Review

---
+++ db/models/Task.js
@@ -0,0 +1,25 @@
+const SequelizeSlugify = require("sequelize-slugify");
+
+module.exports = (sequelize, DataTypes) => {
+  const Task = sequelize.define("Task", {
+    name: { type: DataTypes.STRING, allowNull: false },
+    slug: { type: DataTypes.STRING, unique: true },
+    status: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+    },
+    priority: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: "middle",
+    },
+    deadline: { type: DataTypes.DATE, allowNull: false },
+  });
+
+  SequelizeSlugify.slugifyModel(Donut, {
+    source: ["name"],
+  });
+
+  return Task;
+};
